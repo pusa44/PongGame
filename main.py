25e6f23f6e8b240b75c1956cfefb5c0706c25187
Pusa Khasia <pusa@Pusas-MacBook-Air.local>
2023-06-23 10:40:47 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import ScoreBoard
+import time
+
+paddle_l = Paddle((-350, 0))
+paddle_r = Paddle((350, 0))
+ball = Ball()
+scoreboard = ScoreBoard()
+
+screen = Screen()
+screen.title("Pong Game")
+screen.bgcolor("black")
+screen.setup(800, 600)
+screen.tracer(0)
+
+
+screen.listen()
+screen.onkey(paddle_r.go_up, "Up")
+screen.onkey(paddle_r.go_down, "Down")
+screen.onkey(paddle_l.go_up, "w")
+screen.onkey(paddle_l.go_down, "s")
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(paddle_r) < 50 and ball.xcor() > 320 or ball.distance(paddle_l) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
